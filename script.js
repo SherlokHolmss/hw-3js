efefef47357b65b2a2a5c906bc78037e13b0048f
Davud <david.poninka@gmail.com>
2024-07-11 15:33:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let result = 5 + 5 +"5";
+console.log(typeof(result));
+
+let email = "example@gmail.com";
+if(email.includes("@") === true){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+console.log(email.length);
+
+let word1 = 'My';
+let word2 = 'name';
+let word3 = 'is';
+let fullName = `${word1} ${word2} ${word3} Davyd`;
+console.log(fullName);
+
+userName = 'Volodumyr';
+payment = 300;
+console.log(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
